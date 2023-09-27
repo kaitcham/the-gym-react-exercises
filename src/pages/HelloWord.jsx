cddfe3ec678657716790e47e3bbf47f4b6e80198
kaitcham <chams4f@gmail.com>
2023-09-27 13:03:32 +0200

    Exercise one solution

---
+++ src/pages/HelloWord.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWord = () => {
+  return (
+    <div className="grid h-screen place-content-center bg-yellow-500 text-4xl">
+      Hello Word
+    </div>
+  );
+};
+
+export default HelloWord;
